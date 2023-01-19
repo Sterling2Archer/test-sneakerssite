77742aaedaf6a4da6dac0ae9e2bc266a2c5c0352
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-01-19 17:04:34 -0500

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,17 @@
+route
+
+import React from 'react';
+import Mainpage from '../components/Mainpage';
+import Footer from '../components/Footer'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Mainpage />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
