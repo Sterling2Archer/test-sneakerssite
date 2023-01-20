ece298290007429f9f5e36a4871c02099152f7d5
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-01-19 19:20:33 -0500

    Create Mainpage.js

---
+++ OLD FILES/Mainpage.js
@@ -0,0 +1,61 @@
+component 
+
+.Mainpage {
+    text-align: center;
+    background-color: #ac1212;
+    display: flex;
+    border-top: 300px black solid;
+    border-bottom: 300px black solid;
+  }
+  
+  .App-logo {
+    height: 65vmin;
+    padding: 1rem;
+    pointer-events: none;
+    content: center;
+  }
+  
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin;
+      animation-duration: 8s;
+    }
+  }
+  
+  .App-header {
+    background-color: #ca2525;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: rgb(0, 0, 0);
+  }
+  
+  .App-link {
+    color: #61dafb;
+  }
+  
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  
+  h1 {
+    font-style: italic;
+    font-family: serif;
+    padding: 0rem;
+    font-size: 1cm;
+  }
+  
+  h2 {
+    font-style: italic;
+    font-family: serif;
+    padding: 0rem;
+  }
+  
