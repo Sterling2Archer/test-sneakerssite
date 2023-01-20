400c683687fad1f0477537ce066ab3520c602734
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2023-01-19 19:19:58 -0500

    Create Home.js

---
+++ OLD FILES/Home.js
@@ -0,0 +1,53 @@
+routes - home
+
+import React from 'react';
+import Mainpage from '../components/Mainpage';
+import Footer from '../components/Footer'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Mainpage />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
+
+import React from 'react';
+import Orders from '../components/Orders';
+import Footer from '../components/Footer'
+
+
+const Buy = () => {
+  return (
+    <div>
+      <Orders />
+      <Footer />
+    </div>
+  )
+}
+
+export default Buy;
+
+
+import React from 'react';
+import Second from '../components/Second';
+import Footer from '../components/Footer';
+import SecondCards from '../components/Second';
+
+
+
+const About = () => {
+  return (
+    <div>
+      <Second />
+      <SecondCards />
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
